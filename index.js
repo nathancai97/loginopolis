a78db7498a9873d07e021a25051714366bab8a48
Nathan Cai <nathan97cai@gmail.com>
2023-04-26 23:44:58 -0400

    implemented register and login functionality with bcrypt

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { User } = require('./db');
+const { User } = require("./db");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
-    res.send('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
+    res.send(
+      "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+    );
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({ username, password: hashedPassword });
+    res.status(200).send("successfully created user bobbysmiles");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(401).send("incorrect username or password");
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).send("incorrect username or password");
+    }
+    res.status(200).send(`successfully logged in user ${username}`);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
